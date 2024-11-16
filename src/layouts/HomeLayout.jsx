11a259e274f389edd604af97866938cab4a5aa04
Sheikh Farid <mdfaridsagor101@gmail.com>
2024-11-16 13:29:41 +0600

    added left navbar component

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import Navbar from "../components/Navbar";
+import LeftNavbar from "../layout-components/LeftNavbar";
 
 const HomeLayout = () => {
   return (
@@ -14,8 +15,13 @@ const HomeLayout = () => {
       <nav className="w-11/12 mx-auto py-2">
         <Navbar></Navbar>
       </nav>
-      <main>
-        
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12">
+      <aside className="md:col-span-3">
+        <LeftNavbar></LeftNavbar>
+      </aside>
+      <section className="md:col-span-6">main content</section>
+      <aside className="md:col-span-3">right content</aside>
+
       </main>
     </div>
   );
